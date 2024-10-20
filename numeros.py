0edda00c990e780a8b611161dd9214b799e9b127
Rodrigo Ferreira <rodrigoferreira.mkt@gmial.com>
2024-10-20 16:12:05 -0300

    Números

---
+++ numeros.py
@@ -0,0 +1,11 @@
+print(1 + 2) #SOMA
+
+print(2 - 1) #SUBTRAÇÃO
+
+print(2 * 2) #MULTIPLICAÇÃO
+
+print(2 / 2) #DIVISÃO
+
+print(3 ** 2) #ELEVAR NÚMERO AO QUADRADO
+
+print(4 ** (1/2)) #RAIZ QUADRADA DE 4
