3523f6841afb2b910c25d40b3bc5a3bab6697a47
Rodrigo Ferreira <rodrigoferreira.mkt@gmial.com>
2024-10-20 16:29:31 -0300

    Strings e Indexação

---
+++ strings_index.py
@@ -0,0 +1,13 @@
+minha_string = "Hello World"
+
+print(minha_string)
+print(len(minha_string))
+
+print(minha_string[0])
+
+print(minha_string[1:5])
+print(minha_string[1:])
+
+print(minha_string[-1])
+
+print(minha_string[::2])
